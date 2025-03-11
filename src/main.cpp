be1c66e69e835dfaf6f22c8abce7e5989e41729c
Kacper Gajko <kacper.gajko1@icloud.com>
2025-03-11 21:49:08 +0100

    glfw create window

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,41 @@
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <bgfx/bgfx.h>
+#include <bgfx/platform.h>
 #include <iostream>
-int main() { std::cout << "Hello world!"; }
+
+void error_callback(int error, const char *description) {
+  std::cerr << "GLFW Error " << error << ": " << description << std::endl;
+}
+
+int main() {
+  // Initialize GLFW
+  if (!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW\n";
+    return -1;
+  }
+
+  // Use Metal API
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW_FALSE); // Optional
+
+  // Create GLFW window
+  GLFWwindow *window =
+      glfwCreateWindow(800, 600, "Water renderer", nullptr, nullptr);
+  if (!window) {
+    std::cerr << "Failed to create window\n";
+    glfwTerminate();
+    return -1;
+  }
+
+  // Main loop
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+  }
+
+  // Cleanup
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return 0;
+}
