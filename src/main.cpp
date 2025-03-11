fd642ad60f923df7d7f3ad2fc6e14cfbb459be86
Kacper Gajko <kacper.gajko1@icloud.com>
2025-03-11 22:11:30 +0100

    working glfwGetCocoaWindow(window);

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
-#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+
 #include <bgfx/bgfx.h>
 #include <bgfx/platform.h>
+#include <bx/bx.h>
+
+#if BX_PLATFORM_LINUX
+#define GLFW_EXPOSE_NATIVE_X11
+#elif BX_PLATFORM_WINDOWS
+#define GLFW_EXPOSE_NATIVE_WIN32
+#elif BX_PLATFORM_OSX
+#define GLFW_EXPOSE_NATIVE_COCOA
+#endif
+
+#include "GLFW/glfw3native.h"
+
 #include <iostream>
 
 void error_callback(int error, const char *description) {
@@ -24,6 +36,19 @@ int main() {
       glfwCreateWindow(800, 600, "Water renderer", nullptr, nullptr);
   if (!window) {
     std::cerr << "Failed to create window\n";
+    glfwTerminate();
+    return -1;
+  }
+
+  bgfx::Init init;
+  init.type = bgfx::RendererType::Metal;
+  init.platformData.nwh = glfwGetCocoaWindow(window);
+  init.resolution.width = 1000;
+  init.resolution.height = 800;
+  init.resolution.reset = BGFX_RESET_VSYNC;
+
+  if (!bgfx::init(init)) {
+    std::cerr << "Failed to initialize BGFX!" << std::endl;
     glfwTerminate();
     return -1;
   }
