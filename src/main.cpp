59bf8d44edda0597852d23ea6f54785451a7ea95
Kacper Gajko <kacper.gajko1@icloud.com>
2025-04-26 15:56:05 +0200

    optimize sim data struct

--- src/main.cpp
+++ src/main.cpp
@@ -10,28 +10,51 @@
 #include "renderer/window.hpp"
 
 struct SimulationData {
-  UniformBuffer &u_gravityStatus;
-  UniformBuffer &u_gravity;
-  UniformBuffer &u_resolution;
-  UniformBuffer &u_randomSeed;
-  UniformBuffer &u_mouse;
-  UniformBuffer &u_mouseStrength;
-  UniformBuffer &u_mouseRadius;
-  UniformBuffer &u_numPoints;
-  UniformBuffer &u_radius;
-  UniformBuffer &u_pressureMultiplier;
-  UniformBuffer &u_targetPressure;
+  UniformBuffer u_gravityStatus;
+  UniformBuffer u_gravity;
+  UniformBuffer u_resolution;
+  UniformBuffer u_randomSeed;
+  UniformBuffer u_mouse;
+  UniformBuffer u_mouseStrength;
+  UniformBuffer u_mouseRadius;
+  UniformBuffer u_numPoints;
+  UniformBuffer u_radius;
+  UniformBuffer u_pressureMultiplier;
+  UniformBuffer u_targetPressure;
+  UniformBuffer u_particleSize;
+  UniformBuffer u_particleColorLow;
+  UniformBuffer u_particleColorHigh;
 
-  ComputeBuffer &particleBuffer;
-  ComputeBuffer &densitiesBuffer;
-  ComputeBuffer &velocitiesBuffer;
-  ComputeBuffer &predictionsBuffer;
+  ComputeBuffer particleBuffer;
+  ComputeBuffer densitiesBuffer;
+  ComputeBuffer velocitiesBuffer;
+  ComputeBuffer predictionsBuffer;
 
-  ComputeProgram &posGenProgram;
-  ComputeProgram &step1Program;
-  ComputeProgram &step2Program;
-  ComputeProgram &step3Program;
-  ComputeProgram &step4Program;
+  ComputeProgram posGenProgram;
+  ComputeProgram step1Program;
+  ComputeProgram step2Program;
+  ComputeProgram step3Program;
+  ComputeProgram step4Program;
+
+  SimulationData(int numParticles)
+      : u_gravityStatus("u_gravityStatus"), u_gravity("u_gravity"),
+        u_resolution("u_resolution"), u_randomSeed("u_randomSeed"),
+        u_mouse("u_mouse"), u_mouseStrength("u_mouseStrength"),
+        u_mouseRadius("u_mouseRadius"), u_numPoints("u_numParticles"),
+        u_radius("u_radius"), u_pressureMultiplier("u_pressureMultiplier"),
+        u_targetPressure("u_targetPressure"), u_particleSize("u_particleSize"),
+        u_particleColorLow("u_particleColorLow"),
+        u_particleColorHigh("u_particleColorHigh"),
+
+        particleBuffer(0, "particleBuffer", bgfx::Access::ReadWrite),
+        densitiesBuffer(2, "densitiesBuffer", bgfx::Access::ReadWrite),
+        velocitiesBuffer(3, "velocitiesBuffer", bgfx::Access::ReadWrite),
+        predictionsBuffer(4, "predictionsBuffer", bgfx::Access::ReadWrite),
+        posGenProgram("src/shaders/posGen.compute.bin", numParticles, 1, 1),
+        step1Program("src/shaders/step1.compute.bin", numParticles, 1, 1),
+        step2Program("src/shaders/step2.compute.bin", numParticles, 1, 1),
+        step3Program("src/shaders/step3.compute.bin", numParticles, 1, 1),
+        step4Program("src/shaders/step4.compute.bin", numParticles, 1, 1) {}
 };
 
 void reset(Gui &gui, SimulationData &sim) {
@@ -97,50 +120,10 @@ int main() {
   VertexBuffer vbo(0, "screenVBO");
   IndexBuffer ibo("screenIBO");
 
-  ComputeBuffer particleBuffer(0, "particleBuffer", bgfx::Access::ReadWrite);
-  ComputeBuffer densitiesBuffer(2, "densitiesBuffer", bgfx::Access::ReadWrite);
-  ComputeBuffer velocitiesBuffer(3, "velocitiesBuffer",
-                                 bgfx::Access::ReadWrite);
-  ComputeBuffer predictionsBuffer(4, "predictionsBuffer",
-                                  bgfx::Access::ReadWrite);
-
-  UniformBuffer u_numPoints("u_numParticles");
-  UniformBuffer u_radius("u_radius");
-  UniformBuffer u_resolution("u_resolution");
-  UniformBuffer u_particleSize("u_particleSize");
-  UniformBuffer u_randomSeed("u_randomSeed");
-  UniformBuffer u_pressureMultiplier("u_pressureMultiplier");
-  UniformBuffer u_particleColorLow("u_particleColorLow");
-  UniformBuffer u_particleColorHigh("u_particleColorHigh");
-  UniformBuffer u_targetPressure("u_targetPressure");
-  UniformBuffer u_gravityStatus("u_gravityStatus");
-  UniformBuffer u_gravity("u_gravity");
-  UniformBuffer u_mouse("u_mouse");
-  UniformBuffer u_mouseStrength("u_mouseStrength");
-  UniformBuffer u_mouseRadius("u_mouseRadius");
-
   ShaderProgram shaderProgram("src/shaders/shader.vertex.bin",
                               "src/shaders/shader.fragment.bin");
 
-  ComputeProgram posGenProgram("src/shaders/posGen.compute.bin",
-                               gui.numParticles, 1, 1);
-  ComputeProgram step1Program("src/shaders/step1.compute.bin", gui.numParticles,
-                              1, 1);
-  ComputeProgram step2Program("src/shaders/step2.compute.bin", gui.numParticles,
-                              1, 1);
-  ComputeProgram step3Program("src/shaders/step3.compute.bin", gui.numParticles,
-                              1, 1);
-  ComputeProgram step4Program("src/shaders/step4.compute.bin", gui.numParticles,
-                              1, 1);
-
-  SimulationData sim = {
-      u_gravityStatus,      u_gravity,        u_resolution,
-      u_randomSeed,         u_mouse,          u_mouseStrength,
-      u_mouseRadius,        u_numPoints,      u_radius,
-      u_pressureMultiplier, u_targetPressure, particleBuffer,
-      densitiesBuffer,      velocitiesBuffer, predictionsBuffer,
-      posGenProgram,        step1Program,     step2Program,
-      step3Program,         step4Program};
+  SimulationData sim(gui.numParticles);
 
   reset(gui, sim);
 
@@ -174,15 +157,15 @@ int main() {
 
     vbo.bind();
     ibo.bind();
-    u_numPoints.bindUniform(gui);
-    u_radius.bindUniform(gui);
-    u_resolution.bindUniform(gui);
-    u_particleSize.bindUniform(gui);
-    u_pressureMultiplier.bindUniform(gui);
-    u_particleColorLow.bindUniform(gui);
-    u_particleColorHigh.bindUniform(gui);
-    particleBuffer.bind();
-    velocitiesBuffer.bind();
+    sim.u_numPoints.bindUniform(gui);
+    sim.u_radius.bindUniform(gui);
+    sim.u_resolution.bindUniform(gui);
+    sim.u_particleSize.bindUniform(gui);
+    sim.u_pressureMultiplier.bindUniform(gui);
+    sim.u_particleColorLow.bindUniform(gui);
+    sim.u_particleColorHigh.bindUniform(gui);
+    sim.particleBuffer.bind();
+    sim.velocitiesBuffer.bind();
     shaderProgram.submit();
 
     gui.render();
