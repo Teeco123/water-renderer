4eff39fa2d3a7a9c2de176f968ee098891238927
Kacper Gajko <kacper.gajko1@icloud.com>
2025-05-20 11:30:18 +0200

    updated build.sh for modular shaderc

--- build.sh
+++ build.sh
@@ -6,13 +6,15 @@ make -C build
 
 rm -rf build/bin/src
 
-shaderc -f src/shaders/posGen.compute -o src/shaders/posGen.compute.bin -i lib/bgfx-cmake/bgfx/src --platform osx -p metal --type compute
-shaderc -f src/shaders/step1.compute -o src/shaders/step1.compute.bin -i lib/bgfx-cmake/bgfx/src --platform osx -p metal --type compute
-shaderc -f src/shaders/step2.compute -o src/shaders/step2.compute.bin -i lib/bgfx-cmake/bgfx/src --platform osx -p metal --type compute
-shaderc -f src/shaders/step3.compute -o src/shaders/step3.compute.bin -i lib/bgfx-cmake/bgfx/src --platform osx -p metal --type compute
-shaderc -f src/shaders/step4.compute -o src/shaders/step4.compute.bin -i lib/bgfx-cmake/bgfx/src --platform osx -p metal --type compute
-shaderc -f src/shaders/shader.vertex -o src/shaders/shader.vertex.bin -i lib/bgfx-cmake/bgfx/src --platform osx -p metal --type vertex
-shaderc -f src/shaders/shader.fragment -o src/shaders/shader.fragment.bin -i lib/bgfx-cmake/bgfx/src --platform osx -p metal --type fragment
+SHADERC=./lib/bgfx-cmake/build/cmake/bgfx/shaderc
+
+$SHADERC -f src/shaders/posGen.compute -o src/shaders/posGen.compute.bin -i lib/bgfx-cmake/bgfx/src --platform osx -p metal --type compute
+$SHADERC -f src/shaders/step1.compute -o src/shaders/step1.compute.bin -i lib/bgfx-cmake/bgfx/src --platform osx -p metal --type compute
+$SHADERC -f src/shaders/step2.compute -o src/shaders/step2.compute.bin -i lib/bgfx-cmake/bgfx/src --platform osx -p metal --type compute
+$SHADERC -f src/shaders/step3.compute -o src/shaders/step3.compute.bin -i lib/bgfx-cmake/bgfx/src --platform osx -p metal --type compute
+$SHADERC -f src/shaders/step4.compute -o src/shaders/step4.compute.bin -i lib/bgfx-cmake/bgfx/src --platform osx -p metal --type compute
+$SHADERC -f src/shaders/shader.vertex -o src/shaders/shader.vertex.bin -i lib/bgfx-cmake/bgfx/src --platform osx -p metal --type vertex
+$SHADERC -f src/shaders/shader.fragment -o src/shaders/shader.fragment.bin -i lib/bgfx-cmake/bgfx/src --platform osx -p metal --type fragment
 
 mkdir build/bin/src
 cp -r src/shaders build/bin/src/shaders/
