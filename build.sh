73395e68b5df944e9309b73c114f4355f3352c31
Kacper Gajko <kacper.gajko1@icloud.com>
2025-05-20 12:26:14 +0200

    updated path to shaderc in build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ make -C build
 
 rm -rf build/bin/src
 
-SHADERC=./lib/bgfx-cmake/build/cmake/bgfx/shaderc
+SHADERC=./build/lib/bgfx-cmake/cmake/bgfx/shaderc
 
 $SHADERC -f src/shaders/posGen.compute -o src/shaders/posGen.compute.bin -i lib/bgfx-cmake/bgfx/src --platform osx -p metal --type compute
 $SHADERC -f src/shaders/step1.compute -o src/shaders/step1.compute.bin -i lib/bgfx-cmake/bgfx/src --platform osx -p metal --type compute
