35d87d33a87fccf4ed922363d35960c59bbe9978
Kacper Gajko <kacper.gajko1@icloud.com>
2025-05-20 11:01:44 +0200

    crossplatform (?)

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -1,14 +1,42 @@
 #include "renderer.hpp"
 
 #if BX_PLATFORM_LINUX
+#define GLFW_EXPOSE_NATIVE_WAYLAND
 #define GLFW_EXPOSE_NATIVE_X11
-#elif BX_PLATFORM_WINDOWS
-#define GLFW_EXPOSE_NATIVE_WIN32
+#define GLFW_EXPOSE_NATIVE_GLX
 #elif BX_PLATFORM_OSX
 #define GLFW_EXPOSE_NATIVE_COCOA
+#define GLFW_EXPOSE_NATIVE_NSGL
+#elif BX_PLATFORM_WINDOWS
+#define GLFW_EXPOSE_NATIVE_WIN32
+#define GLFW_EXPOSE_NATIVE_WGL
 #endif
 
 #include "GLFW/glfw3native.h"
+
+static void *glfwNativeWindowHandle(GLFWwindow *_window) {
+#if BX_PLATFORM_LINUX
+  if (GLFW_PLATFORM_WAYLAND == glfwGetPlatform()) {
+    return glfwGetWaylandWindow(_window);
+  }
+
+  return (void *)uintptr_t(glfwGetX11Window(_window));
+#elif BX_PLATFORM_OSX
+  return glfwGetCocoaWindow(_window);
+#elif BX_PLATFORM_WINDOWS
+  return glfwGetWin32Window(_window);
+#endif // BX_PLATFORM_
+}
+
+static bgfx::RendererType::Enum glfwNativeRendererType() {
+#if BX_PLATFORM_LINUX
+  return bgfx::RendererType::Vulkan;
+#elif BX_PLATFORM_OSX
+  return bgfx::RendererType::Metal;
+#elif BX_PLATFORM_WINDOWS
+  return bgfx::RendererType::Direct3D12;
+#endif
+}
 
 Renderer::Renderer(GLFWwindow *window, int width, int height)
     : width(width), height(height) {
@@ -16,8 +44,8 @@ Renderer::Renderer(GLFWwindow *window, int width, int height)
 
   // Create metal renderer
   bgfx::Init init;
-  init.type = bgfx::RendererType::Metal;
-  init.platformData.nwh = glfwGetCocoaWindow(window);
+  init.type = glfwNativeRendererType();
+  init.platformData.nwh = glfwNativeWindowHandle(window);
   init.resolution.width = width;
   init.resolution.height = height;
   init.resolution.reset = BGFX_RESET_VSYNC;
